ca357a62e0daa72564bdddd399d101982648c3c2
Uzair Shamim <uzashamim@gmail.com>
2023-09-25 23:36:30 -0400

    Remove tests

--- TwitchEverywhere.UnitTests/TwitchConnector/TwitchConnectorTests.cs
+++ TwitchEverywhere.UnitTests/TwitchConnector/TwitchConnectorTests.cs
@@ -1,3 +1,6 @@
+using System.Net.WebSockets;
+using System.Text;
+using Moq;
 using TwitchEverywhere.Implementation;
 
 namespace TwitchEverywhere.UnitTests.TwitchConnector;
@@ -5,7 +8,52 @@ namespace TwitchEverywhere.UnitTests.TwitchConnector;
 [TestFixture]
 public class TwitchConnectorTests {
     private ITwitchConnector m_twitchConnector;
-    private IWebSocketConnection m_webSocketConnection;
+    private Mock<IAuthorizer> m_authorizer;
+    private Mock<IWebSocketConnection> m_webSocketConnection;
+    
+    [SetUp]
+    public void Setup() {
+        m_authorizer = new Mock<IAuthorizer>( MockBehavior.Strict );
+        m_webSocketConnection = new Mock<IWebSocketConnection>( MockBehavior.Strict );
+
+        m_authorizer
+            .Setup( auth => auth.GetToken() )
+            .ReturnsAsync( "hello" );
+
+        m_webSocketConnection
+            .Setup( 
+                ws => 
+                    ws.ConnectAsync(
+                        new Uri("ws://irc-ws.chat.twitch.tv:80"), 
+                        CancellationToken.None
+                    )
+            ).Returns( Task.CompletedTask );
+        
+        m_webSocketConnection
+            .Setup( 
+                ws => 
+                    ws.SendAsync(
+                        It.IsAny<ArraySegment<byte>>(),
+                        WebSocketMessageType.Text, 
+                        true, 
+                        CancellationToken.None
+                    )
+            ).Returns( Task.CompletedTask );
+
+        m_webSocketConnection.Setup( ws => ws.State )
+            .Returns( WebSocketState.Open );
+        
+        m_twitchConnector = new Implementation.TwitchConnector( 
+            authorizer: m_authorizer.Object,
+            webSocketConnection: m_webSocketConnection.Object
+        );
+    }
+
+    [Test]
+    public async Task ValidOptions_Connect_ReturnsTrue() {
+        bool result = await m_twitchConnector.TryConnect( m_options, m_callback );
+        Assert.That( result, Is.True );
+    }
     
     private readonly TwitchConnectionOptions m_options = new(
         Channel: "TestConnection",
@@ -21,25 +69,4 @@ public class TwitchConnectorTests {
     ) {
         Assert.That( s, Is.Not.Null );
     };
-    
-    [SetUp]
-    public void Setup() {
-        IAuthorizer authorizer = new Authorizer(
-            accessToken: m_options.AccessToken ?? "",
-            refreshToken: m_options.RefreshToken ?? "",
-            clientId: m_options.ClientId ?? "",
-            clientSecret: m_options.ClientSecret ?? ""
-        );
-
-        m_twitchConnector = new Implementation.TwitchConnector( 
-            authorizer: authorizer,
-            webSocketConnection: m_webSocketConnection
-        );
-    }
-
-    [Test]
-    public async Task ValidOptions_Connect_ReturnsTrue() {
-        bool result = await m_twitchConnector.TryConnect( m_options, m_callback );
-        Assert.That( result, Is.True );
-    }
 }
