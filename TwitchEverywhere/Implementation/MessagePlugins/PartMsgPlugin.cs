2a12d48aff8eb063a3dcc5c0684e872f7d862669
Uzair Shamim <uzashamim@gmail.com>
2023-10-21 01:16:17 -0400

    Add PART message

---
+++ TwitchEverywhere/Implementation/MessagePlugins/PartMsgPlugin.cs
@@ -0,0 +1,23 @@
+using TwitchEverywhere.Types;
+using TwitchEverywhere.Types.Messages;
+
+namespace TwitchEverywhere.Implementation.MessagePlugins; 
+
+public class PartMsgPlugin : IMessagePlugin {
+
+    bool IMessagePlugin.CanHandle(
+        string response,
+        string channel
+    ) {
+        return response.Contains( $" PART #{channel}" );
+    }
+    Message IMessagePlugin.GetMessageData(
+        string response,
+        string channel
+    ) {
+        return new PartMsg( 
+            message: response, 
+            channel: channel 
+        );
+    }
+}
