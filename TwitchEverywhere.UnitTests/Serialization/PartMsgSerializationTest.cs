a18e00b3495da88c14f752415c95a5a40cd6625a
Uzair Shamim <uzashamim@gmail.com>
2023-12-08 23:46:42 -0500

    Add Notice and Part message tests

---
+++ TwitchEverywhere.UnitTests/Serialization/PartMsgSerializationTest.cs
@@ -0,0 +1,38 @@
+using System.Collections.Immutable;
+using TwitchEverywhere.Types;
+using TwitchEverywhere.Types.Messages.ImmediateLoadedMessages;
+using TwitchEverywhere.Types.Messages.Interfaces;
+using TwitchEverywhere.Types.Messages.LazyLoadedMessages;
+
+namespace TwitchEverywhere.UnitTests.Serialization;
+
+[TestFixture]
+public class PartMsgSerializationTest {
+
+    [Test]
+    public void LazyLoadedPartMsgSerialization() {
+        const string channel = "channel";
+        const string message =
+            ":ronni!ronni@ronni.tmi.twitch.tv PART #dallas";
+        LazyLoadedPartMsg lazyLoadedPartMsg = new( channel: channel, message: message );
+
+        Assert.That( lazyLoadedPartMsg.RawMessage, Is.EqualTo( message ) );
+    }
+
+    [Test]
+    public void MessageDeletedSuccessfully_ImmediateLoadedPartMsg_SerializationToIRCMessage() {
+
+        ImmediateLoadedPartMsg immediateLoadedPartMsg = new(
+            channel: "dallas",
+            user: "ronni"
+        );
+
+        Assert.That(
+            immediateLoadedPartMsg.RawMessage,
+            Is.EqualTo(
+                ":ronni!ronni@ronni.tmi.twitch.tv PART #dallas"
+            )
+        );
+    }
+    
+}
--- TwitchEverywhere/Types/Messages/ImmediateLoadedMessages/ImmediateLoadedPartMsg.cs
+++ TwitchEverywhere/Types/Messages/ImmediateLoadedMessages/ImmediateLoadedPartMsg.cs
@@ -5,25 +5,36 @@ namespace TwitchEverywhere.Types.Messages.ImmediateLoadedMessages;
 
 public class ImmediateLoadedPartMsg : IPartMsg {
     private readonly string m_message;
-    private string m_channel;
-    private string m_user;
+    private readonly string m_channel;
+    private readonly string m_user;
 
     public ImmediateLoadedPartMsg(
-        string message,
         string channel,
-        string user
+        string? message = null,
+        string? user = null
     ) {
-        m_message = message;
         m_channel = channel;
-        m_user = user;
+        m_message = message ?? string.Empty;
+        m_user = user ?? string.Empty;
     }
 
 
     public MessageType MessageType => MessageType.Part;
-    public string RawMessage => m_message;
+    public string RawMessage => GetRawMessage();
     public string Channel => m_channel;
 
     string IPartMsg.User => m_user;
 
+    private string GetRawMessage() {
+        return $":{m_user}!{m_user}@{m_user}.tmi.twitch.tv {MessageType.ToString().ToUpper()} #{m_channel}";
+    }
+
+    private string SerializeProperty(
+        MessagePluginUtils.Properties property,
+        Func<string> serializer
+    ) {
+
+        return string.Format( MessagePluginUtils.GetPropertyAsString( property ), serializer() );
+    }
 
 }
